aa7c02ee3ae1d5c9e921f3a1eb8b3ea0fb9238c8
amashigeseiji <amashige@tejimaya.com>
2014-06-14 19:48:05 +0900

    flashNotice, flashErrorをパーシャルに分離

---
+++ apps/pc_frontend/templates/_flash.php
@@ -0,0 +1,6 @@
+<?php if ($sf_user->hasFlash('error')): ?>
+<?php op_include_parts('alertBox', 'flashError', array('body' => __($sf_user->getFlash('error'), $sf_data->getRaw('sf_user')->getFlash('error_params', array())))) ?>
+<?php endif; ?>
+<?php if ($sf_user->hasFlash('notice')): ?>
+<?php op_include_parts('alertBox', 'flashNotice', array('body' => __($sf_user->getFlash('notice'), $sf_data->getRaw('sf_user')->getFlash('notice_params', array())))) ?>
+<?php endif; ?>
