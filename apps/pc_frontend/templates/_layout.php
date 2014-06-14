8b1bbaf63dbb1194a1abbc0300c39142d944f4a0
amashigeseiji <amashige@tejimaya.com>
2014-06-14 20:26:35 +0900

    htmlの構造がわかりづらいためインデントを付ける

--- apps/pc_frontend/templates/_layout.php
+++ apps/pc_frontend/templates/_layout.php
@@ -2,73 +2,78 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
 <head>
-<?php include_partial('global/html_header', array('sf_user' => $sf_user, 'op_config' => $op_config)) ?>
+  <?php include_partial('global/html_header', array('sf_user' => $sf_user, 'op_config' => $op_config)) ?>
 </head>
 <body id="<?php printf('page_%s_%s', $view->getModuleName(), $view->getActionName()) ?>" class="<?php echo opToolkit::isSecurePage() ? 'secure_page' : 'insecure_page' ?>">
-<?php echo $op_config->get('pc_html_top2') ?>
-<div id="Body">
-<?php echo $op_config->get('pc_html_top') ?>
-<div id="Container">
-<div id="Header" class="navbar <?php if('friend' == getNavType()): ?>navbar-inverse<?php endif; ?>">
-<div id="HeaderContainer" class="navbar-inner">
-<div class="container">
-<?php include_partial('global/header') ?>
-</div>
-</div><!-- HeaderContainer -->
-</div><!-- Header -->
 
-<div id="Contents">
-<div id="ContentsContainer">
+  <?php echo $op_config->get('pc_html_top2') ?>
 
-<div id="Layout<?php echo $layout ?>" class="Layout">
+  <div id="Body">
 
-<?php include_partial('global/flash', array('sf_user' => $sf_user, 'sf_data' => $sf_data)) ?>
+    <?php echo $op_config->get('pc_html_top') ?>
 
-<?php if (has_slot('op_top')): ?>
-<div id="Top">
-<?php include_slot('op_top') ?>
-</div><!-- Top -->
-<?php endif; ?>
+    <div id="Container">
 
-<?php if (has_slot('op_sidemenu')): ?>
-<div id="Left">
-<?php include_slot('op_sidemenu') ?>
-</div><!-- Left -->
-<?php endif; ?>
+      <div id="Header" class="navbar <?php if('friend' == getNavType()): ?>navbar-inverse<?php endif; ?>">
+        <div id="HeaderContainer" class="navbar-inner">
+          <div class="container">
+            <?php include_partial('global/header') ?>
+          </div>
+        </div><!-- HeaderContainer -->
+      </div><!-- Header -->
 
-<div id="Center">
-<?php echo $sf_content ?>
-</div><!-- Center -->
+      <div id="Contents">
+        <div id="ContentsContainer">
 
-<?php if (has_slot('op_bottom')): ?>
-<div id="Bottom">
-<?php include_slot('op_bottom') ?>
-</div><!-- Bottom -->
-<?php endif; ?>
+          <div id="Layout<?php echo $layout ?>" class="Layout">
+            <?php include_partial('global/flash', array('sf_user' => $sf_user, 'sf_data' => $sf_data)) ?>
 
-</div><!-- Layout -->
+            <?php if (has_slot('op_top')): ?>
+              <div id="Top">
+                <?php include_slot('op_top') ?>
+              </div><!-- Top -->
+            <?php endif; ?>
 
-<div id="sideBanner">
-<?php include_component('default', 'sideBannerGadgets'); ?>
+            <?php if (has_slot('op_sidemenu')): ?>
+              <div id="Left">
+                <?php include_slot('op_sidemenu') ?>
+              </div><!-- Left -->
+            <?php endif; ?>
 
-</div><!-- sideBanner -->
+            <div id="Center">
+              <?php echo $sf_content ?>
+            </div><!-- Center -->
 
-</div><!-- ContentsContainer -->
-</div><!-- Contents -->
+            <?php if (has_slot('op_bottom')): ?>
+              <div id="Bottom">
+                <?php include_slot('op_bottom') ?>
+              </div><!-- Bottom -->
+            <?php endif; ?>
+          </div><!-- Layout -->
 
-<?php if ($sf_request->isSmartphone(false)): ?>
-<?php include_partial('default/SmtSwitch') ?>
-<?php endif ?>
+          <div id="sideBanner">
+            <?php include_component('default', 'sideBannerGadgets'); ?>
+          </div><!-- sideBanner -->
 
-<div id="Footer">
-<div id="FooterContainer">
-<?php include_partial('global/footer') ?>
-</div><!-- FooterContainer -->
-</div><!-- Footer -->
+        </div><!-- ContentsContainer -->
+      </div><!-- Contents -->
 
-<?php echo $op_config->get('pc_html_bottom2') ?>
-</div><!-- Container -->
-<?php echo $op_config->get('pc_html_bottom') ?>
-</div><!-- Body -->
+      <?php if ($sf_request->isSmartphone(false)): ?>
+        <?php include_partial('default/SmtSwitch') ?>
+      <?php endif ?>
+
+      <div id="Footer">
+        <div id="FooterContainer">
+          <?php include_partial('global/footer') ?>
+        </div><!-- FooterContainer -->
+      </div><!-- Footer -->
+
+      <?php echo $op_config->get('pc_html_bottom2') ?>
+
+    </div><!-- Container -->
+
+  <?php echo $op_config->get('pc_html_bottom') ?>
+
+  </div><!-- Body -->
 </body>
 </html>
