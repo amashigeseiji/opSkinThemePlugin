0212cbe0704c74ad61e958482dc9549831078939
amashigeseiji <amashige@tejimaya.com>
2014-06-15 17:07:56 +0900

    無意味な分岐を削除
    @member_profile_mineページのナビであれば分岐するようになっているが,
    出力されるはずの$memberNameが設定されていない(無意味であるとおもわれるので削除する)。

--- apps/pc_frontend/modules/default/templates/_globalNav.php
+++ apps/pc_frontend/modules/default/templates/_globalNav.php
@@ -7,11 +7,7 @@
 <ul class="dropdown-menu">
 <?php foreach ($navs as $nav): ?>
 <?php if (op_is_accessible_url($nav->uri)): ?>
-<?php if('@member_profile_mine' === $nav->uri): ?>
-<li id="globalNav_<?php echo op_url_to_id($nav->uri, true) ?>"><?php echo link_to($memberName, $nav->uri) ?></li>
-<?php else: ?>
 <li id="globalNav_<?php echo op_url_to_id($nav->uri, true) ?>"><?php echo link_to($nav->caption, $nav->uri) ?></li>
-<?php endif ?>
 <?php endif; ?>
 <?php endforeach; ?>
 </ul>
